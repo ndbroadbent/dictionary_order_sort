75772db8590b119e2ee8d740eb265fe5cd26aa01
Nathan Broadbent <nathan.f77@gmail.com>
2012-11-09 16:00:13 +1300

    Added docs to method [ci skip]

--- lib/dictionary_order_sort/array.rb
+++ lib/dictionary_order_sort/array.rb
@@ -1,4 +1,7 @@
 class Array
+  # Sorts an array of strings with 'dictionary sort' logic.
+  # Produces the same order as the Linux command `sort --dictionary-order`.
+  # (Note that `--dictionary-order` is the default order for the `sort` command.)
   def dict_sort
     sorting_cache = {}
     sort do |a, b|
